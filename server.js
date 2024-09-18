f712766bd83ea4804928703e29a79b0371ff51de
Pranavsolanki01 <pranavsolanki200@gmail.com>
2024-09-18 21:21:08 +0530

    adding server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const errorHandler = require("./middleware/errorhandler");
+const dotenv = require("dotenv").config();
+const connectDB = require("./config/dbConnection");
+
+connectDB();
+
+const app = express();
+app.use(express.json());
+
+const port = process.env.PORT || 9090;
+app.listen(port, () => {
+  console.log(`server started on port ${port}`);
+});
+
+app.use("/api/contacts", require("./routes/contactRoutes"));
+
+app.use(errorHandler);
+
+// app.get("/api/contact", );
